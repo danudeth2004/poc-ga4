5db0f604cb2c8ef72e88c4ca66d2283d1006ceba
danudeth2004 <oat25482547@gmail.com>
2025-12-26 19:07:47 +0700

    make sure for set user_id before send another event

--- app/javascript/controllers/ga4_controller.js
+++ app/javascript/controllers/ga4_controller.js
@@ -1,10 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["timeSpent"];
-
   connect() {
-    const userId = this.element.getAttribute("data-ga-user-id");
+    const userId = this.element.getAttribute("data-ga4-user-id");
     if (userId && typeof window.gtag === "function") {
       gtag("set", { user_id: userId, debug_mode: true });
     }
